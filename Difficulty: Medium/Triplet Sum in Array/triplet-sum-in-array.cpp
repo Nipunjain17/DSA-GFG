6dc54126567002d370342becebcaa050ea7a0103
Nipunjain17 <nipunjain504@gmail.com>
2025-08-29 11:54:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Triplet Sum in Array/triplet-sum-in-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    bool hasTripletSum(vector<int> &arr, int target) {
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<n-2;i++){
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                int sum = arr[i]+arr[j]+arr[k];
+                if(sum == target){
+                    return true;
+                }
+                else if(sum < target){
+                    j++;
+                }
+                else{
+                    k--;
+                }
+            }
+        }
+        return false;
+    }
+};
