1183cf8b6769cdd7ab6515d70b1ba5b4309c079f
Nipunjain17 <nipunjain504@gmail.com>
2025-09-28 21:42:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int maxProduct(vector<int> &arr) {
+        int n = arr.size();
+        
+        if(n == 0){
+            return 0;
+        }
+        
+        int maxi = arr[0] , maxp = arr[0] , minp = arr[0];
+        for(int i=1;i<n;i++){
+            int cp = maxp;
+            
+            maxp = max({arr[i] , arr[i] * maxp , arr[i] * minp});
+            minp = min({arr[i] , cp * arr[i] , arr[i] * minp});
+            
+            maxi = max(maxi,maxp);
+        }
+        return maxi;
+    }
+};
