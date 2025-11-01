b016849dc81466f1c246dc55cb7c374f3cbff7e2
Nipunjain17 <nipunjain504@gmail.com>
2025-11-01 10:35:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Third largest element/third-largest-element.cpp
+++ Difficulty: Easy/Third largest element/third-largest-element.cpp
@@ -2,12 +2,24 @@ class Solution {
   public:
     int thirdLargest(vector<int> &arr) {
         int n = arr.size();
-        sort(arr.begin(),arr.end());
-        if(n >= 3){
-            return arr[n-3];
-        }
-        else{
+        
+        if(n < 3){
             return -1;
         }
+        
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        
+        for(int i=0; i<3; i++){
+            minHeap.push(arr[i]);
+        }
+        
+        for(int i=3; i<n; i++){
+            if(arr[i] > minHeap.top()){
+                minHeap.pop();
+                minHeap.push(arr[i]);
+            }
+        }
+        
+        return minHeap.top();
     }
 };
