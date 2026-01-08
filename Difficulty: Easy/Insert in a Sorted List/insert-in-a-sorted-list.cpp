1ef00054a65c6a44178397744d8be2daee11d763
Nipunjain17 <nipunjain504@gmail.com>
2026-01-08 21:59:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insert in a Sorted List/insert-in-a-sorted-list.cpp
@@ -0,0 +1,33 @@
+/*
+structure of the node of the list is as
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    // Should return head of the modified linked list
+    Node* sortedInsert(Node* head, int key) {
+        Node* newNode = new Node(key);
+        if(head == NULL ||key <= head->data){ // for first position
+            newNode->next = head;
+            return newNode;
+        }
+        
+        Node* curr = head;
+        while(curr->next != NULL && curr->next->data < key){
+            curr = curr->next;
+        }
+        newNode->next = curr->next;
+        curr->next = newNode;
+        return head;
+    }
+};
