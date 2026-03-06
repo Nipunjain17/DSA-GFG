a66a003b245bb97a943e9179265999420ce935cb
Nipunjain17 <nipunjain504@gmail.com>
2026-03-06 17:15:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Column name from a given column number/column-name-from-a-given-column-number.cpp
@@ -0,0 +1,17 @@
+
+
+class Solution {
+  public:
+    string colName(long long int n) {
+        string ans = "";
+        
+        while(n>0){
+            n--;
+            char ch = 'A' +(n % 26);
+            ans = ch + ans;
+            n = n/26;
+        }
+        
+        return ans;
+    }
+};
